2f68846794d96d9e42b83487d800c52c6e3964be
Leonardo Bueno <leobueno.dev@gmail.com>
2022-11-03 16:34:03 -0300

    🎉 Exemplo funcional de drag and drop

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<template>
+  <div 
+    class="drop-zone"
+    @drop="onDrop($event, 1)"
+    @dragenter.prevent
+    @dragover.prevent
+  >
+    <div 
+      class="drag-el" 
+      v-for="item in getList(1)" 
+      :key="item.id"
+      draggable="true"
+      @dragstart="startDrag($event, item)"
+    >
+      {{ item.title }}
+    </div>
+  </div>
+  <div 
+    class="drop-zone"
+    @drop="onDrop($event, 2)"
+    @dragenter.prevent
+    @dragover.prevent
+  >
+    <div 
+      class="drag-el" 
+      v-for="item in getList(2)" 
+      :key="item.id"
+      draggable="true"
+      @dragstart="startDrag($event, item)"
+    >
+      {{ item.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const items = ref([
+      { id: 0, title: 'Item A', list: 1 },
+      { id: 1, title: 'Item B', list: 1 },
+      { id: 2, title: 'Item C', list: 2 },
+    ])
+
+    const getList = (list) => {
+      return items.value.filter((item) => item.list === list)
+    }
+
+    const startDrag = (event, item) => {
+      console.log('start drag', item)
+      event.dataTransfer.dropEffect = 'move'
+      event.dataTransfer.effectAllowed = 'move'
+      event.dataTransfer.setData('itemID', item.id)
+    }
+
+    const onDrop = (event, list) => {
+      const itemID = event.dataTransfer.getData('itemID')
+      const item = items.value.find((item) => item.id == itemID)
+      item.list = list
+      console.log('on drop', itemID)
+
+    }
+
+    return {
+      getList,
+      startDrag,
+      onDrop
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.drop-zone {
+  width: 200px;
+  height: 200px;
+  border: 1px solid black;
+  margin: 10px;
+  padding: 10px;
+  min-height: 10px;
+}
+
+.drag-el {
+  background-color: #3498db;
+  color: white;
+  padding: 5px;
+  margin-bottom: 10px;
+}
+</style>
